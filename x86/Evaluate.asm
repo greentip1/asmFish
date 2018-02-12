3a1eb3b546c68232b864898ed5962537d440e496
Counterply <counterply@gmail.com>
2018-02-11 18:15:34 -0600

    Fix makefile errors

--- x86/Evaluate.asm
+++ x86/Evaluate.asm
@@ -1,21 +1,21 @@
-
-MinorBehindPawn 	= (( 16 shl 16) + (  0))
-BishopPawns		= ((  8 shl 16) + ( 12))
-RookOnPawn		= ((  8 shl 16) + ( 24))
-TrappedRook		= (( 92 shl 16) + (  0))
-WeakQueen		= (( 50 shl 16) + ( 10))
-OtherCheck		= (( 10 shl 16) + ( 10))
-CloseEnemies		= ((  7 shl 16) + (  0))
-PawnlessFlank		= (( 20 shl 16) + ( 80))
-ThreatByHangingPawn	= (( 71 shl 16) + ( 61))
-ThreatBySafePawn        = ((175 shl 16) + (168))
-ThreatByRank		= (( 16 shl 16) + (  3))
-Hanging 		= (( 52 shl 16) + ( 30))
-ThreatByPawnPush	= (( 47 shl 16) + ( 26))
-HinderPassedPawn	= ((  8 shl 16) + (  1))
-LongRangedBishop        =  (22 shl 16)  +  0
-TrappedBishopA1H1       =  (50 shl 16)  + 50
-
+MinorBehindPawn 	= ( 16 shl 16) + (  0)
+BishopPawns		= (  8 shl 16) + ( 12)
+LongRangedBishop        = ( 22 shl 16) + (  0)
+RookOnPawn		= (  8 shl 16) + ( 24)
+TrappedRook		= ( 92 shl 16) + (  0)
+WeakQueen		= ( 50 shl 16) + ( 10)
+OtherCheck		= ( 10 shl 16) + ( 10)
+CloseEnemies		= (  7 shl 16) + (  0)
+PawnlessFlank		= ( 20 shl 16) + ( 80)
+ThreatByHangingPawn	= ( 71 shl 16) + ( 61)
+ThreatBySafePawn        = (192 shl 16) + (175)
+ThreatByRank		= ( 16 shl 16) + (  3)
+Hanging 		= ( 48 shl 16) + ( 27)
+WeakUnopposedPawn       = (  5 shl 16) + ( 25)
+ThreatByPawnPush	= ( 38 shl 16) + ( 22)
+ThreatByAttackOnQueen   = ( 38 shl 16) + ( 22)
+HinderPassedPawn	= (  7 shl 16) + (  0)
+TrappedBishopA1H1       = ( 50 shl 16) + ( 50)
 
 LazyThreshold = 1500
 
@@ -873,8 +873,8 @@ macro EvalThreats Us
   local ThreatMinorLoop, ThreatMinorDone, ThreatRookLoop, ThreatRookDone
   local ThreatMinorSkipPawn, ThreatRookSkipPawn
 
-        ThreatByKing0   = (( 3 shl 16) + ( 65))
-        ThreatByKing1   = (( 9 shl 16) + (145))
+        ThreatByKing0   = (( 3 shl 16) + ( 62))
+        ThreatByKing1   = (( 9 shl 16) + (138))
 
   if Us = White
 	;addsub		equ add

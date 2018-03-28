770c89882f60382db46251db7deea17ca0e3e55b
Counterply <counterply@gmail.com>
2018-03-28 04:02:50 -0500

    make.bat

--- make.bat
+++ make.bat
@@ -7,12 +7,21 @@ set "datestamp=%YYYY%-%MM%-%DD%" & set "timestamp=%HH%%Min%%Sec%" & set "fullsta
 "asmFish\fasm.exe" "asmFish\asmFishW_base.asm"      -m 50000 "Windows\asmFishW_%datestamp%_base.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_popcnt.asm"    -m 50000 "Windows\asmFishW_%datestamp%_popcnt.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_bmi2.asm"      -m 50000 "Windows\asmFishW_%datestamp%_bmi2.exe"
-"asmFish\fasm.exe" "asmFish\pedantFishW_base.asm"   -m 50000 "Windows\pedantFishW_%datestamp%_base.exe"
-"asmFish\fasm.exe" "asmFish\pedantFishW_popcnt.asm" -m 50000 "Windows\pedantFishW_%datestamp%_popcnt.exe"
-"asmFish\fasm.exe" "asmFish\pedantFishW_bmi2.asm"   -m 50000 "Windows\pedantFishW_%datestamp%_bmi2.exe"
-"asmFish\fasm.exe" "asmFish\asmFishL_base.asm"      -m 50000 "Linux\asmFishL_%datestamp%_base"
-"asmFish\fasm.exe" "asmFish\asmFishL_popcnt.asm"    -m 50000 "Linux\asmFishL_%datestamp%_popcnt"
-"asmFish\fasm.exe" "asmFish\asmFishL_bmi2.asm"      -m 50000 "Linux\asmFishL_%datestamp%_bmi2"
-"asmFish\fasm.exe" "asmFish\pedantFishL_base.asm"   -m 50000 "Linux\pedantFishL_%datestamp%_base"
-"asmFish\fasm.exe" "asmFish\pedantFishL_popcnt.asm" -m 50000 "Linux\pedantFishL_%datestamp%_popcnt"
-"asmFish\fasm.exe" "asmFish\pedantFishL_bmi2.asm"   -m 50000 "Linux\pedantFishL_%datestamp%_bmi2"
+
+set "dir=C:\GitHub\Counterply\asmFish\"
+
+copy %dir%\Windows\asmFishW_%datestamp%_bmi2.exe %dir%\asmFishW_%datestamp%_bmi2.exe
+
+
+REM "asmFish\fasm.exe" "asmFish\asmFishW_base.asm"      -m 50000 "Windows\asmFishW_%datestamp%_base.exe"
+REM "asmFish\fasm.exe" "asmFish\asmFishW_popcnt.asm"    -m 50000 "Windows\asmFishW_%datestamp%_popcnt.exe"
+
+REM "asmFish\fasm.exe" "asmFish\pedantFishW_base.asm"   -m 50000 "Windows\pedantFishW_%datestamp%_base.exe"
+REM "asmFish\fasm.exe" "asmFish\pedantFishW_popcnt.asm" -m 50000 "Windows\pedantFishW_%datestamp%_popcnt.exe"
+REM "asmFish\fasm.exe" "asmFish\pedantFishW_bmi2.asm"   -m 50000 "Windows\pedantFishW_%datestamp%_bmi2.exe"
+REM "asmFish\fasm.exe" "asmFish\asmFishL_base.asm"      -m 50000 "Linux\asmFishL_%datestamp%_base"
+REM "asmFish\fasm.exe" "asmFish\asmFishL_popcnt.asm"    -m 50000 "Linux\asmFishL_%datestamp%_popcnt"
+REM "asmFish\fasm.exe" "asmFish\asmFishL_bmi2.asm"      -m 50000 "Linux\asmFishL_%datestamp%_bmi2"
+REM "asmFish\fasm.exe" "asmFish\pedantFishL_base.asm"   -m 50000 "Linux\pedantFishL_%datestamp%_base"
+REM "asmFish\fasm.exe" "asmFish\pedantFishL_popcnt.asm" -m 50000 "Linux\pedantFishL_%datestamp%_popcnt"
+REM "asmFish\fasm.exe" "asmFish\pedantFishL_bmi2.asm"   -m 50000 "Linux\pedantFishL_%datestamp%_bmi2"

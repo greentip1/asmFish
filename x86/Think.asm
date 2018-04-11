abc259e0320d3ca5223e92a548c2c6495e822104
Counterply <counterply@gmail.com>
2018-04-11 09:26:25 -0500

    Revert "Merge pull request #152 from tthsqe12/master"
    This reverts commit fa1382d565f38127edf16fb6d381ddadfd37f58a, reversing
    changes made to 2c6066f73bf55fcb83613ebcd1b5bcdc0312ae34.

--- x86/Think.asm
+++ x86/Think.asm
@@ -342,7 +342,7 @@ end if
 		mov   qword[.elapsed], rax
 
             xor  r10d, r10d
-           test  r12d, r12d     ; cmp  r12d, VALUE_DRAW
+			cmp  r12d, VALUE_DRAW
             jne  @1f
 	    mov  edx, dword[rbp + Pos.sideToMove]
             mov  ecx, dword[limits.time + 4*rdx]
@@ -506,14 +506,8 @@ MainThread_Think:
 		                        ; eax holds contempt value (ignore remainder in edx)
 		                        ; Next, we construct mg/eg contempt score
 		mov   ecx,eax           ; ecx represents mg value now (i.e., the contempt value just calculated)
-
-
-;		shr   eax, 1            ; contempt / 2 => weaken the contempt for the endgame (eg value)
-                cdq             ; this 
-                sub  eax, edx   ; divides eax by 2
-                sar  eax, 1     ; as a SIGNED dword
-
-		shl   ecx, 16           ; move mg contempt into upper 16 bits (this is opposite to current master)
+		shr   eax, 1            ; contempt / 2 => weaken the contempt for the endgame (eg value)
+		shl   ecx, 16           ; move mg contempt into upper 16 bits
 		add   ecx, eax          ; layer in eg value to get overall "Score"
 
 		mov   eax, dword[rbp+Pos.sideToMove]
@@ -524,7 +518,7 @@ MainThread_Think:
 		neg   ecx
 		
 .save_contempt_score:
-                mov   dword[Eval_Contempt], ecx
+		mov dword[ContemptScore], ecx
 		add   byte[mainHash.date], 4
 
 if USE_WEAKNESS
